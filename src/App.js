3fd94d5a4a482f9619dcea99e7a5558212f9821b
marodi-mykhailo <mykhailo.marodi@gmail.com>
2020-06-08 18:47:57 +0200

    stworzylem reducer dla książek, stworzylem store, dodalem loggera

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {setBooks} from "./actions/books";
 
-function App() {
-  return (
-    <div className='container'>
-s
-    </div>
-  );
+
+
+class App extends Component{
+  render() {
+    const { books } = this.props.books;
+    const {setBooks} = this.props;
+    const newBooks =[
+        {
+            id: 0,
+            title: 'shopping cart' + " " + new Date()
+        }
+    ];
+    return(
+        <div>
+          <h1>{books[0].title}</h1>
+            <button onClick={setBooks.bind(this, newBooks)}>SET NEW BOOKS</button>
+        </div>
+    )
+  }
+
 }
 
-export default App;
+const mapStateToProps = state =>({
+    ...state
+});
+
+const mapDispatchToProps = dispatch =>({
+    setBooks: books => dispatch(setBooks(books))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {Provider} from 'react-redux';
+
 import App from "./App";
 
-ReactDOM.render( <App /> , document.getElementById('root'));
+import createStore from "./store";
+const store = createStore();
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
